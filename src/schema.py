b15462df16e83121738dea1b9e3779a7006ebb17
jorgemoraes <jdemoraes.jr@gmail.com>
2024-08-31 11:19:26 -0300

    crud local docker

---
+++ src/schema.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import Union
+
+
+class ItemBase(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
